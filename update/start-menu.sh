10eae1ba6c675f780cddef2f70e016aafbe939b9
sarwenda <142522186+saputribosen@users.noreply.github.com>
2023-08-18 06:33:12 +0700

    Update start-menu.sh

--- update/start-menu.sh
+++ update/start-menu.sh
@@ -130,5 +130,5 @@ fi
 # echo -e " Xray/V2Ray              :$RED [Error] $NC"
 # fi
 echo -e "$BLUE━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━$NC"
-echo -e "$BRED----------- Mod By LIZSVR -----------"
+echo -e "$BRED----------- Mod By ARYO -----------"
 echo -e "$BLUE━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━$NC"
