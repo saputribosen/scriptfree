93cd55dda9d2aae7134a513e8e48e5c26a043c9c
sarwenda <142522186+saputribosen@users.noreply.github.com>
2023-08-21 21:21:17 +0700

    Update backup.sh

--- backup/backup.sh
+++ backup/backup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# My Telegram : https://t.me/Akbar218
+# My Telegram : https://t.me/Opindoo
 # ==========================================
 # Color
 RED='\033[0;31m'
@@ -48,12 +48,12 @@ cp /etc/gshadow backup/
 #cp -r /etc/wireguard backup/wireguard
 #cp /etc/ppp/chap-secrets backup/chap-secrets
 #cp /etc/ipsec.d/passwd backup/passwd1
-#cp /etc/shadowsocks-libev/akun.conf backup/ss.conf
-#cp -r /var/lib/airavpn/ backup/airavpn
+cp /etc/shadowsocks-libev/akun.conf backup/ss.conf
+cp -r /var/lib/airavpn/ backup/airavpn
 #cp -r /home/sstp backup/sstp
 cp -r /etc/xray backup/xray/
-#cp -r /etc/trojan-go backup/trojan-go
-#cp -r /usr/local/shadowsocksr/ backup/shadowsocksr
+cp -r /etc/trojan-go backup/trojan-go
+cp -r /usr/local/shadowsocksr/ backup/shadowsocksr
 cp -r /home/vps/public_html backup/public_html
 cd /root
 zip -r $IP-$date.zip backup > /dev/null 2>&1
